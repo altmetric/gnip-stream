a7976f76e455c4ac503e0b5d13aee1810d6c8f7d
Scott Matthewman <scott@altmetric.com>
2015-08-10 15:26:29 +0100

    Use brace matching to determine when to split up JSON data
    On some inputs, most notably the compliance firehose data, it appeared
    that splitting input on each '\r\n' combination meant that individual
    methods actually included more than one JSON object.
    
    This fix uses a brace-matching Regex to grab JSON packets based on
    them always beginning and ending with `{` and `}`, and with any
    opening braces within also having a matching closing brace.
    
    By looking for this combination of factors, each element of the array
    returned by `complete entries` should be a JSON object. It also means
    that multiple `\r\n` sequences, for example as sent by GNIP to keep
    the connection alive, do not get sent on.

--- lib/gnip-stream/json_data_buffer.rb
+++ lib/gnip-stream/json_data_buffer.rb
@@ -1,9 +1,9 @@
 module GnipStream
   class JsonDataBuffer
-    attr_accessor :split_pattern, :check_pattern
-    def initialize(split_pattern, check_pattern)
-      @split_pattern = split_pattern
-      @check_pattern = check_pattern
+    JSON_BRACE_MATCHING = /(?<match>\{(?:\g<match>|[^{}]++)*\})(?<excess>.*\Z)/m
+    attr_accessor :buffer
+
+    def initialize
       @buffer = ''
     end
 
@@ -13,10 +13,9 @@ module GnipStream
 
     def complete_entries
       entries = []
-      while @buffer =~ check_pattern
-        activities = @buffer.split(split_pattern)
-        entries << activities.shift
-        @buffer = activities.join(split_pattern)
+      while (look_for_json = @buffer.match(JSON_BRACE_MATCHING))
+        entries << look_for_json[:match]
+        @buffer = look_for_json[:excess]
       end
 
       entries
