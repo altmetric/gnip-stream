18c3039eec1e61b4ab26b83e9d221f312e1d1e28
Scott Matthewman <scott@altmetric.com>
2015-08-13 14:08:36 +0100

    Throw a custom exception if the stream buffer gets full
    JIRA: https://digital-science.atlassian.net/browse/TWIT-26
    
    While the new `JsonExtractor` should prevent the buffer from
    continually growing unchecked, any repeat occurrences should notify
    us rather than continuing to grow unchecked.
    
    This commit adds a default buffer size of 20,000 characters. Analysis
    of live tweet data coming in from GNIP saw some packets rising to
    7,500 characters in length (exact packet size varies depending on
    GNIP enrichment data, tweet and profile length, etc.) so this limit
    would allow 2-3 tweets to build up in the buffer before erroring out.
    In practice the buffer's incoming packets are 1 tweet long so it
    should never even approach this value.

--- lib/gnip-stream/json_data_buffer.rb
+++ lib/gnip-stream/json_data_buffer.rb
@@ -1,22 +1,28 @@
+require 'gnip-stream/errors'
 require 'gnip-stream/json_extractor'
 
 module GnipStream
   class JsonDataBuffer
     attr_accessor :buffer
+    attr_reader :maximum_size
+    private :maximum_size
 
-    def initialize
+    def initialize(maximum_size: 20_000)
       @buffer = ''
+      @maximum_size = maximum_size
     end
 
     def process(chunk)
-      @buffer.concat(chunk)
+      buffer.concat(chunk)
+
+      fail GnipStream::BufferFullError, "Buffer is: #{buffer}" if buffer.size > maximum_size
     end
 
     def complete_entries
       parser = JsonExtractor.new(@buffer)
       parser.process
 
-      @buffer = parser.suffix
+      self.buffer = parser.suffix
       parser.matches
     end
   end
